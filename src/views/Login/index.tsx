9602a93a5e1befda6cfdd10d68b129e1ceadb142
huangbuff <14409742+huang-shouhua-123@user.noreply.gitee.com>
2025-11-12 12:56:29 +0800

    chron:react降级，login搭建

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,44 @@
-export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+import React, { useCallback } from 'react';
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+import styles from './Login.module.less';
+type LoginFormType = {
+    username?: string;
+    password?: string;
+};
+const Login: React.FC<FormProps> = () => {
+    const onFinish = useCallback((values: LoginFormType) => {
+        console.log('Success:', values);
+    }, []);
+    return (
+        <div className={styles.loginForm}>
+            <Form
+                name="login"
+                labelCol={{ span: 8 }}
+                onFinish={onFinish}
+                autoComplete="off"
+            >
+                <h2 className={styles.formTitle}>欢迎登录</h2>
+                <Form.Item<LoginFormType>
+                    name="username"
+                    rules={[{ required: true, message: '请输入账号' }]}
+                >
+                    <Input placeholder="请输入账号" />
+                </Form.Item>
+                <Form.Item<LoginFormType>
+                    name="password"
+                    rules={[{ required: true, message: '请输入密码' }]}
+                >
+                    <Input.Password placeholder="请输入密码" />
+                </Form.Item>
+                <Form.Item >
+                    <Button type="primary" htmlType="submit" block>
+                        登录
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div >
+
+    );
 }
+export default Login;
