37c198d49995aaf2dc50e86c221961f30c38da7f
huangbuff <14409742+huang-shouhua-123@user.noreply.gitee.com>
2025-11-13 11:33:34 +0800

    fix:miants完善redux配置

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,20 @@ import Login from './views/Login'
 import { APP_ROUTES } from './constants/routes.ts'
 import { ConfigProvider } from 'antd';
 import { THEME_CONFIG } from './constants/theme.ts'
-
+import store from '@/store'
+import { Provider } from 'react-redux'
 import Loading from './components/Loading'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfigProvider theme={THEME_CONFIG}>
-      <HashRouter>
-        <Routes>
-          <Route path={APP_ROUTES.LOGIN} element={<Suspense fallback={<Loading />}> <Login /> </Suspense>} />
-          <Route path="*" element={<Suspense fallback={<Loading />}> <App /> </Suspense>} />
-        </Routes>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <Routes>
+            <Route path={APP_ROUTES.LOGIN} element={<Suspense fallback={<Loading />}> <Login /> </Suspense>} />
+            <Route path="*" element={<Suspense fallback={<Loading />}> <App /> </Suspense>} />
+          </Routes>
+        </HashRouter>
+      </Provider>
     </ConfigProvider>
   </StrictMode>
 )
